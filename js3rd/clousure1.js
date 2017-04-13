5c78d09746832d5dfbc44fa52cf0eebb6b34acc7
huker <huaolie@sina.com>
2017-04-13 23:53:15 +0800

    0413

---
+++ js3rd/clousure1.js
@@ -0,0 +1,12 @@
+/**
+ * Created by huk on 17/4/5.
+ */
+
+// for(var i=0;i<5;i++){
+//     (function (j) {
+//         setTimeout(function () {
+//             console.log(j)
+//         },1000*j)
+//     })(i)
+// }
+
---
+++ js3rd/sort.js
@@ -0,0 +1,4 @@
+/**
+ * Created by huk on 17/4/6.
+ */
+var arr = [1,9,2,7,3,2]
